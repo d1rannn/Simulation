3618c5b5e605854615e7bbac79179c1438aca4f5
d1rannn <denchik.stars06@gmail.com>
2024-07-01 06:49:45 +0500

    Change perform method

--- src/actions/MoveCreaturesAction.java
+++ src/actions/MoveCreaturesAction.java
@@ -4,15 +4,22 @@ import creature.Creature;
 import map.Cell;
 import map.WorldMap;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class MoveCreaturesAction extends Action {
 
     @Override
     public void perform(WorldMap map) {
+        Set<Cell> processedCells = new HashSet<>();
+
         for (int i = 0; i < map.getHeight(); i++) {
             for (int j = 0; j < map.getWidth(); j++) {
                 Cell cell = new Cell(i, j);
-                if (map.getEntity(cell) instanceof Creature) {
-                    ((Creature) map.getEntity(cell)).makeMove(map, cell);
+                if (!processedCells.contains(cell) && map.getEntity(cell) instanceof Creature creature) {
+                    Cell targetCell = creature.makeMove(map, cell);
+                    processedCells.add(targetCell);
+                    processedCells.add(cell);
                 }
             }
         }
